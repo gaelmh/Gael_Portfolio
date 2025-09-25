5943c5d2824a8544d2601cc11084b862e9943e5b
Gael Mota Hernandez <153122845+gaelmh@users.noreply.github.com>
2025-09-25 18:34:42 -0500

    Added script.js for redirection

---
+++ script.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirecting...</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>You are being redirected...</h1>
+    </header>
+    <main>
+        <p>This site has moved! You will be automatically redirected to the new location in <span id="countdown">5</span> seconds.</p>
+        <p>If you are not redirected, please click the link below:</p>
+        <p><a href="https://www.example.com">Go to the new site now</a></p>
+    </main>
+
+    <script src="script.js"></script>
+</body>
+</html>
